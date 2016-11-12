64d135fd91371bdb16e8b440a28f864a89421306
Federico Vaga <federico.vaga@vaga.pv.it>
2016-11-12 23:54:01 +0100

    add installation script and documentation
    Signed-off-by: Federico Vaga <federico.vaga@vaga.pv.it>

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyWordreference',
+      version='1.0',
+      description='Python module to access wordreference dictionaries',
+      author='Federico Vaga',
+      author_email='federico.vaga@vaga.pv.it',
+      maintainer="Federico Vaga",
+      maintainer_email="federico.vaga@vaga.pv.it",
+      url='https://github.com/FedericoVaga/PyWordReference',
+      packages=['PyWordReference'],
+      license='GPLv3',
+     )
